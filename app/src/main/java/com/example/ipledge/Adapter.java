b046374c3362962764f88b73b35b78c89951a6d8
Brinda Moudgalya <brinda.moudgalya@gmail.com>
2023-10-14 16:48:59 -0700

    just images as cards, cards full width - change width of cards

--- app/src/main/java/com/example/ipledge/Adapter.java
+++ app/src/main/java/com/example/ipledge/Adapter.java
@@ -12,19 +12,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     LayoutInflater inflater;
-    List<String> storeName, plantName, plantPrice, plantURL;
+    List<String> plantURL;
 
-    public Adapter(Context context, List<String> storeName, List<String> plantName,
-                   List<String> plantPrice, List<String> plantURL){
+    public Adapter(Context context, List<String> plantURL){
         this.inflater= LayoutInflater.from(context);
-        this.storeName = storeName;
-        this.plantName = plantName;
-        this.plantPrice = plantPrice;
         this.plantURL = plantURL;
 
 //        Log.d("TAG", "Adapter: " + plantName);
@@ -39,25 +37,19 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String store = storeName.get(position);
-        String plant = plantName.get(position);
-        String price = plantPrice.get(position);
         String plantUrl = plantURL.get(position);
 
         // load text into individual textViews
-        holder.store.setText(store);
-        holder.plant.setText(plant);
-        holder.price.setText(price);
 
         // load picture into imageView for the picture
 //        Picasso.get().load("https://i.imgur.com/DvpvklR.png").into(holder.image);
-//        Picasso.get().load(plantUrl).into(holder.image);
+        Picasso.get().load(plantUrl).into(holder.image);
 
     }
 
     @Override
     public int getItemCount() {
-        return storeName.size();
+        return plantURL.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
@@ -65,10 +57,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         ImageView image;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            store = itemView.findViewById(R.id.plantStoreTextView);
-            plant = itemView.findViewById(R.id.plantNameTextView);
-            price = itemView.findViewById(R.id.plantPriceTextView);
-            image = itemView.findViewById(R.id.pictureOfPlant);
+            image = itemView.findViewById(R.id.pictureOfCompany);
         }
     }
 }
