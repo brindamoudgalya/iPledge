446bf04e4429382016a841d58d64a77209c8b7ad
Brinda Moudgalya <brinda.moudgalya@gmail.com>
2023-10-14 16:18:49 -0700

    initial commit

---
+++ app/src/main/java/com/example/ipledge/Adapter.java
@@ -0,0 +1,74 @@
+package com.example.ipledge;
+
+// reference: https://www.youtube.com/watch?v=kxdVo4RH3nE
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    LayoutInflater inflater;
+    List<String> storeName, plantName, plantPrice, plantURL;
+
+    public Adapter(Context context, List<String> storeName, List<String> plantName,
+                   List<String> plantPrice, List<String> plantURL){
+        this.inflater= LayoutInflater.from(context);
+        this.storeName = storeName;
+        this.plantName = plantName;
+        this.plantPrice = plantPrice;
+        this.plantURL = plantURL;
+
+//        Log.d("TAG", "Adapter: " + plantName);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String store = storeName.get(position);
+        String plant = plantName.get(position);
+        String price = plantPrice.get(position);
+        String plantUrl = plantURL.get(position);
+
+        // load text into individual textViews
+        holder.store.setText(store);
+        holder.plant.setText(plant);
+        holder.price.setText(price);
+
+        // load picture into imageView for the picture
+//        Picasso.get().load("https://i.imgur.com/DvpvklR.png").into(holder.image);
+//        Picasso.get().load(plantUrl).into(holder.image);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return storeName.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView store, plant, price;
+        ImageView image;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            store = itemView.findViewById(R.id.plantStoreTextView);
+            plant = itemView.findViewById(R.id.plantNameTextView);
+            price = itemView.findViewById(R.id.plantPriceTextView);
+            image = itemView.findViewById(R.id.pictureOfPlant);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ipledge/SearchFunctionActivity.java
@@ -0,0 +1,141 @@
+package com.example.ipledge;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import cz.msebera.android.httpclient.Header;
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.read.biff.BiffException;
+
+public class SearchFunctionActivity extends AppCompatActivity {
+
+    private ListView listSearch;
+    private EditText editSearch;
+    private ArrayList<Adapter> adapterArrayList = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter;
+
+    // variables for pulling information from excel spreadsheets
+    // reference: https://www.youtube.com/watch?v=kxdVo4RH3nE
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+    Adapter adapter;
+    AsyncHttpClient client;
+    Workbook workbook;
+    List<String> storeName, plantName, plantPrice, plantURL;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_function);
+
+        // connecting activity to xml components
+        listSearch = findViewById(R.id.listSearch);
+        editSearch = findViewById(R.id.editSearch);
+        progressBar = findViewById(R.id.progressBar);
+
+        addData("");
+
+
+        // setting array adapter, which sets the information displayed in the searchview
+        arrayAdapter = new ArrayAdapter<>
+                (this, R.layout.list_item, R.id.plantNameTextView, plantName);
+
+        editSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            // as user types, plants are deleted if they don't contain correct letters
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // clear recyclerview
+                recyclerView.setAdapter(null);
+                // clear arraylists
+                storeName.clear();
+                plantName.clear();
+                plantPrice.clear();
+                plantURL.clear();
+                addData(charSequence);
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+    }
+    private void addData(CharSequence charSequence) {
+        // code for excel spreadsheet information
+        String url = "https://github.com/brindamoudgalya/MoonGate/blob/master/MoonGateFinalSheet.xls?raw=true";
+        recyclerView = findViewById(R.id.recyclerView);
+
+        storeName = new ArrayList<>();
+        plantName = new ArrayList<>();
+        plantPrice = new ArrayList<>();
+        plantURL = new ArrayList<>();
+
+        client = new AsyncHttpClient();
+        progressBar.setVisibility(View.VISIBLE);
+        client.get(url, new FileAsyncHttpResponseHandler(this) {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(SearchFunctionActivity.this, "Download Failed.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, File file) {
+                progressBar.setVisibility(View.GONE);
+                WorkbookSettings ws = new WorkbookSettings();
+                ws.setGCDisabled(true);
+                if (file != null) {
+                    try {
+                        workbook = Workbook.getWorkbook(file);
+                        Sheet sheet = workbook.getSheet(0);
+                        for (int i = 0; i < sheet.getRows(); i++) {
+                            Cell[] row = sheet.getRow(i);
+                            if (row[0].getContents().toLowerCase(Locale.ROOT).contains(charSequence)
+                                    || row[1].getContents().toLowerCase(Locale.ROOT).contains(charSequence)
+                                    || row[2].getContents().toLowerCase(Locale.ROOT).startsWith("$" + charSequence)) {
+                                storeName.add(row[0].getContents());
+                                plantName.add(row[1].getContents());
+                                plantPrice.add(row[2].getContents());
+                                plantURL.add(row[3].getContents());
+                            }
+                        }
+
+                        showData();
+
+                    } catch (IOException | BiffException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+    private void showData() {
+        adapter = new Adapter(this, storeName, plantName, plantPrice, plantURL);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+}
